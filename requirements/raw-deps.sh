5449a10386b5ea34badb7a6a7496ba738f3d3b40
flaviusburca <flavius.burca@gmail.com>
2025-11-12 04:54:08 +0200

    GPTQModel not needed for now

--- requirements/raw-deps.sh
+++ requirements/raw-deps.sh
@@ -2,4 +2,3 @@ uv pip install "vllm" --pre --torch-backend=auto --extra-index-url https://wheel
 uv pip install "sglang==0.5.5.post1" --pre --no-deps
 uv pip install "ms-swift==3.9.3" --no-deps
 uv pip install "llmcompressor==0.8.1" --no-deps
-uv pip install "GPTQModel==5.4.0" --no-deps
