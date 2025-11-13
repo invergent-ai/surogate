f51b9d9bcd212df725c5df323a54de4f22b25c1f
flaviusburca <flavius.burca@gmail.com>
2025-11-13 07:58:17 +0200

    - add evalscope to nodeps

--- requirements/raw-deps.sh
+++ requirements/raw-deps.sh
@@ -1,4 +1,3 @@
-uv pip install "vllm" --pre --torch-backend=auto --extra-index-url https://wheels.vllm.ai/nightly --no-deps
-uv pip install "sglang==0.5.5.post1" --pre --no-deps
-uv pip install "ms-swift==3.9.3" --no-deps
-uv pip install "llmcompressor==0.8.1" --no-deps
+uv pip install "vllm" --no-deps --pre --torch-backend=auto --extra-index-url https://wheels.vllm.ai/nightly
+uv pip install --no-deps "sglang==0.5.5.post1" "ms-swift==3.9.3" "llmcompressor==0.8.1" "evalscope==1.1.1"
+
