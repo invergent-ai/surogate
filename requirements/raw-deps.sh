30313090eec5db86e2ce410b3c77bc8fb5b1105d
flaviusburca <flavius.burca@gmail.com>
2025-11-17 05:53:25 +0200

    - bump ms-swift

--- requirements/raw-deps.sh
+++ requirements/raw-deps.sh
@@ -1,3 +1,3 @@
 uv pip install "vllm" --no-deps --pre --torch-backend=auto --extra-index-url https://wheels.vllm.ai/nightly
-uv pip install --no-deps "sglang==0.5.5.post1" "ms-swift==3.9.3" "llmcompressor==0.8.1" "evalscope==1.1.1"
+uv pip install --no-deps "sglang==0.5.5.post1" "ms-swift==3.10.1" "llmcompressor==0.8.1" "evalscope==1.1.1"
 
