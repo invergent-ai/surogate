60930de14d78257df3cae370259bdd62e78bcc0d
flaviusburca <flavius@invergent.ai>
2025-12-01 11:47:42 +0000

    - pin vllm dependency

--- requirements/raw-deps.sh
+++ requirements/raw-deps.sh
@@ -1,3 +1,3 @@
-uv pip install "vllm" --no-deps --pre --torch-backend=auto --extra-index-url https://wheels.vllm.ai/nightly
+uv pip install "vllm==0.11.2" --no-deps --pre --torch-backend=auto
 uv pip install --no-deps "sglang==0.5.5.post3" "ms-swift==3.10.3" "llmcompressor==0.8.1" "evalscope==1.1.1"
 uv pip install --no-deps --index-url https://pypi.org/simple --extra-index-url https://test.pypi.org/simple lmcache==0.3.10.dev32
