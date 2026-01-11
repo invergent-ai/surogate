63677f9a4463f286485e4e820f8fed218c564a0b
flaviusburca <flavius.burca@gmail.com>
2026-01-11 18:29:47 +0000

    Fix lora weight initialization & qlora path

--- csrc/src/modules/lora/lora_model_import_export.h
+++ csrc/src/modules/lora/lora_model_import_export.h
@@ -13,10 +13,21 @@ namespace modules {
 
 template<typename Block>
 void ModularLoRAModel<Block>::import_weights(const std::string& file_name, bool allow_cast, NCCLCommunicator& comm) {
-    if (qlora_enabled()) {
-        import_weights_qlora(file_name, comm);
+    if (qlora_enabled() && lora_enabled()) {
+        // QLoRA mode: load base weights into quantized storage, then inject weight provider
+        if (mQLoRAConfig.is_fp4()) {
+            import_weights_fp4_qlora(file_name, comm);
+        } else if (mQLoRAConfig.is_bnb()) {
+            import_weights_bnb_qlora(file_name, comm);
+        } else {
+            import_weights_qlora(file_name, comm);
+        }
     } else {
+        // Standard LoRA or non-LoRA: load base weights normally
         mBaseModel->import_weights(file_name, allow_cast, comm);
+    }
+    if (lora_enabled()) {
+        mLoRAWeights->random_init(42, comm);
     }
 }
 
